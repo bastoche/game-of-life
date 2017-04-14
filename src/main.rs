4983569d71a3ae546b3cb965ef2bc50825a52b3b
Bastien Duret <bastien.duret@gmail.com>
2017-04-14 15:34:39 +0200

    Implementation complete ?

--- src/main.rs
+++ src/main.rs
@@ -30,24 +30,24 @@ impl Grid {
 }
 
 
-fn next_state(state: State, living_neighbours_count: u8) -> State {
+fn next_state(state: &State, living_neighbours_count: u8) -> State {
   match (state, living_neighbours_count) {
-    (State::Dead, 3) => State::Alive,
-    (State::Alive, c) if 2 <= c && c <= 3 => State::Alive,
+    (&State::Dead, 3) => State::Alive,
+    (&State::Alive, c) if 2 <= c && c <= 3 => State::Alive,
     _ => State::Dead
   }
 }
 
-fn living_neighbours_count(cells: Vec<Vec<State>>, x: usize, y: usize) -> u8 {
+fn living_neighbours_count(cells: &Vec<Vec<State>>, x: usize, y: usize) -> u8 {
   let height = cells.len();
   let width = cells[0].len();
   [
-    cell_at(&cells, x - 1, y - 1, width, height),
-    cell_at(&cells, x - 1, y, width, height),
-    cell_at(&cells, x - 1, y + 1, width, height),
-    cell_at(&cells, x, y - 1, width, height),
+    cell_at(&cells, x + width - 1, y + width - 1, width, height),
+    cell_at(&cells, x + width - 1, y, width, height),
+    cell_at(&cells, x + width - 1, y + 1, width, height),
+    cell_at(&cells, x, y + width - 1, width, height),
     cell_at(&cells, x, y + 1, width, height),
-    cell_at(&cells, x + 1, y - 1, width, height),
+    cell_at(&cells, x + 1, y + width - 1, width, height),
     cell_at(&cells, x + 1, y, width, height),
     cell_at(&cells, x + 1, y + 1, width, height),
   ].iter().fold(0, |sum, state| sum + if state == &&State::Alive { 1 } else { 0 })
@@ -58,7 +58,12 @@ fn cell_at(cells: &Vec<Vec<State>>, x: usize, y: usize, width: usize, height: us
 }
 
 fn next_generation(grid: &Grid) -> Grid {
-  Grid { cells: grid.cells.clone() }
+  let cells = grid.cells.iter().enumerate().map(
+    |(y, line)| line.iter().enumerate().map(
+      |(x, state)| next_state(state, living_neighbours_count(&grid.cells, x, y))
+    ).collect()
+  ).collect();
+  Grid { cells: cells }
 }
 
 #[cfg(test)]
@@ -74,28 +79,28 @@ mod tests {
 
   #[test]
   fn a_live_cell_with_less_than_2_living_neighbours_will_die() {
-    assert_eq!(State::Dead, next_state(State::Alive, 0));
-    assert_eq!(State::Dead, next_state(State::Alive, 1));
+    assert_eq!(State::Dead, next_state(&State::Alive, 0));
+    assert_eq!(State::Dead, next_state(&State::Alive, 1));
   }
 
   #[test]
   fn a_live_cell_with_2_or_3_living_neighbours_will_stay_alive() {
-    assert_eq!(State::Alive, next_state(State::Alive, 2));
-    assert_eq!(State::Alive, next_state(State::Alive, 3));
+    assert_eq!(State::Alive, next_state(&State::Alive, 2));
+    assert_eq!(State::Alive, next_state(&State::Alive, 3));
   }
 
   #[test]
   fn a_live_cell_with_more_than_3_living_neighbours_will_die() {
-    assert_eq!(State::Dead, next_state(State::Alive, 4));
-    assert_eq!(State::Dead, next_state(State::Alive, 5));
-    assert_eq!(State::Dead, next_state(State::Alive, 6));
-    assert_eq!(State::Dead, next_state(State::Alive, 7));
-    assert_eq!(State::Dead, next_state(State::Alive, 8));
+    assert_eq!(State::Dead, next_state(&State::Alive, 4));
+    assert_eq!(State::Dead, next_state(&State::Alive, 5));
+    assert_eq!(State::Dead, next_state(&State::Alive, 6));
+    assert_eq!(State::Dead, next_state(&State::Alive, 7));
+    assert_eq!(State::Dead, next_state(&State::Alive, 8));
   }
 
   #[test]
   fn a_dead_cell_with_3_living_neighbours_will_live() {
-    assert_eq!(State::Alive, next_state(State::Dead, 3));
+    assert_eq!(State::Alive, next_state(&State::Dead, 3));
   }
 
   #[test]
@@ -105,20 +110,19 @@ mod tests {
       vec![State::Dead, State::Dead, State::Dead],
       vec![State::Dead, State::Alive, State::Dead]
     ];
-    assert_eq!(3, living_neighbours_count(cells, 1, 1));
+    assert_eq!(3, living_neighbours_count(&cells, 1, 1));
   }
 
   #[test]
   fn next_generation_when_all_cells_are_dead() {
-    let grid = Grid::from_string("...");
+    let grid = Grid::from_string("...\n...\n...");
     assert_eq!(grid, next_generation(&grid));
   }
 
   #[test]
-  #[ignore]
   fn next_generation_for_basic_blinker() {
-    let grid = Grid::from_string("...\n***\n...");
-    let next_generation_grid = Grid::from_string(".*.\n.*.\n.*.");
+    let grid = Grid::from_string(".....\n.....\n.***.\n.....\n.....");
+    let next_generation_grid = Grid::from_string(".....\n..*..\n..*..\n..*..\n.....");
     assert_eq!(next_generation_grid, next_generation(&grid));
   }
 }
