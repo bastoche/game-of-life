6bdaa8081705082408c1c3205dd58b784741d5d8
Bastien Duret <bastien.duret@gmail.com>
2017-04-14 14:59:06 +0200

    Implement rules in next state function

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,15 @@ impl Grid {
   }
 }
 
+
+fn next_state(state: State, living_neighbours_count: u8) -> State {
+  match (state, living_neighbours_count) {
+    (State::Dead, 3) => State::Alive,
+    (State::Alive, c) if 2 <= c && c <= 3 => State::Alive,
+    _ => State::Dead
+  }
+}
+
 fn next_generation(grid: &Grid) -> Grid {
   Grid { cells: grid.cells.clone() }
 }
@@ -38,15 +47,50 @@ mod tests {
   use super::*;
 
   #[test]
+  fn from_string() {
+    let representation = "...";
+    let grid = Grid::from_string(representation);
+    assert_eq!(vec!(vec![State::Dead, State::Dead, State::Dead]), grid.cells);
+  }
+
+  #[test]
+  fn a_live_cell_with_less_than_2_living_neighbours_will_die() {
+    assert_eq!(State::Dead, next_state(State::Alive, 0));
+    assert_eq!(State::Dead, next_state(State::Alive, 1));
+  }
+
+  #[test]
+  fn a_live_cell_with_2_or_3_living_neighbours_will_stay_alive() {
+    assert_eq!(State::Alive, next_state(State::Alive, 2));
+    assert_eq!(State::Alive, next_state(State::Alive, 3));
+  }
+
+  #[test]
+  fn a_live_cell_with_more_than_3_living_neighbours_will_die() {
+    assert_eq!(State::Dead, next_state(State::Alive, 4));
+    assert_eq!(State::Dead, next_state(State::Alive, 5));
+    assert_eq!(State::Dead, next_state(State::Alive, 6));
+    assert_eq!(State::Dead, next_state(State::Alive, 7));
+    assert_eq!(State::Dead, next_state(State::Alive, 8));
+  }
+
+
+  #[test]
+  fn a_dead_cell_with_3_living_neighbours_will_live() {
+    assert_eq!(State::Alive, next_state(State::Dead, 3));
+  }
+
+  #[test]
   fn next_generation_when_all_cells_are_dead() {
     let grid = Grid::from_string("...");
     assert_eq!(grid, next_generation(&grid));
   }
 
   #[test]
-  fn from_string() {
-    let representation = "...";
-    let grid = Grid::from_string(representation);
-    assert_eq!(vec!(vec![State::Dead, State::Dead, State::Dead]), grid.cells);
+  #[ignore]
+  fn next_generation_for_basic_blinker() {
+    let grid = Grid::from_string("...\n***\n...");
+    let next_generation_grid = Grid::from_string(".*.\n.*.\n.*.");
+    assert_eq!(next_generation_grid, next_generation(&grid));
   }
 }
